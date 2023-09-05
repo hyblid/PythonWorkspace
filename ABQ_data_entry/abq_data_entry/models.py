c066da80651059697b073c4723a4db4c944ab0b1
hwang <HHwang@qaconsultants.com>
2023-09-05 10:24:34 -0400

    edit

--- ABQ_data_entry/abq_data_entry/models.py
+++ ABQ_data_entry/abq_data_entry/models.py
@@ -10,48 +10,21 @@ class CSVModel:
 
   fields = {
     "Date": {'req': True, 'type': FT.iso_date_string},
-    "Time": {
-      'req': True, 'type': FT.string_list,
-      'values': ['8:00', '12:00', '16:00', '20:00']
-    },
+    "Time": {'req': True, 'type': FT.string_list,'values': ['8:00', '12:00', '16:00', '20:00']    },
     "Technician": {'req': True, 'type':  FT.string},
-    "Lab": {
-      'req': True, 'type': FT.short_string_list,
-      'values': ['A', 'B', 'C']
-    },
-    "Plot": {
-      'req': True, 'type': FT.string_list,
-      'values': [str(x) for x in range(1, 21)]
-    },
-    "Seed Sample":  {'req': True, 'type': FT.string},
-    "Humidity": {
-      'req': True, 'type': FT.decimal,
-      'min': 0.5, 'max': 52.0, 'inc': .01
-    },
-    "Light": {
-      'req': True, 'type': FT.decimal,
-      'min': 0, 'max': 100.0, 'inc': .01
-    },
-    "Temperature": {
-      'req': True, 'type': FT.decimal,
-      'min': 4, 'max': 40, 'inc': .01
-    },
+    "Lab": {'req': True, 'type': FT.short_string_list,'values': ['A', 'B', 'C']},
+    "Plot": {'req': True, 'type': FT.string_list,'values': [str(x) for x in range(1, 21)]},
+    "Seed Sample":  {'req': True, 'type': FT.string}, 
+    "Humidity": { 'req': True, 'type': FT.decimal, 'min': 0.5, 'max': 52.0, 'inc': .01},
+    "Light": {'req': True, 'type': FT.decimal,'min': 0, 'max': 100.0, 'inc': .01},
+    "Temperature": {'req': True, 'type': FT.decimal,'min': 4, 'max': 40, 'inc': .01},
     "Equipment Fault": {'req': False, 'type': FT.boolean},
     "Plants": {'req': True, 'type': FT.integer, 'min': 0, 'max': 20},
     "Blossoms": {'req': True, 'type': FT.integer, 'min': 0, 'max': 1000},
     "Fruit": {'req': True, 'type': FT.integer, 'min': 0, 'max': 1000},
-    "Min Height": {
-      'req': True, 'type': FT.decimal,
-      'min': 0, 'max': 1000, 'inc': .01
-    },
-    "Max Height": {
-      'req': True, 'type': FT.decimal,
-      'min': 0, 'max': 1000, 'inc': .01
-    },
-    "Med Height": {
-      'req': True, 'type': FT.decimal,
-      'min': 0, 'max': 1000, 'inc': .01
-    },
+    "Min Height": {'req': True, 'type': FT.decimal,'min': 0, 'max': 1000, 'inc': .01},
+    "Max Height": {'req': True, 'type': FT.decimal,'min': 0, 'max': 1000, 'inc': .01},
+    "Med Height": {'req': True, 'type': FT.decimal,'min': 0, 'max': 1000, 'inc': .01},
     "Notes": {'req': False, 'type': FT.long_string}
   }
 
@@ -66,10 +39,8 @@ class CSVModel:
     file_exists = os.access(self.file, os.F_OK)
     parent_writeable = os.access(self.file.parent, os.W_OK)
     file_writeable = os.access(self.file, os.W_OK)
-    if (
-      (not file_exists and not parent_writeable) or
-      (file_exists and not file_writeable)
-    ):
+    if ((not file_exists and not parent_writeable) 
+        or (file_exists and not file_writeable)):
       msg = f'Permission denied accessing file: {filename}'
       raise PermissionError(msg)
 
