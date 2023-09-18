1d6f719e2c99737f8a891db99a2741c4eb0bf048
hwang <HHwang@qaconsultants.com>
2023-09-18 14:14:07 -0400

    finished 21

--- 50_example/21_longest_word.py
+++ 50_example/21_longest_word.py
@@ -1,4 +1,11 @@
 import os
+import hashlib
+import glob
+import arrow as arrow
+import datetime
+import collections as collection
+
+
 def find_longest_word(filename):
     longest_word = ''
     for one_line in open(filename):
@@ -10,6 +17,36 @@ def find_longest_word(filename):
 def find_all_longest_words(dirname):
     return { filename: find_longest_word(os.path.join(dirname,filename))
     for filename in os.listdir(dirname) if os.path.isfile(os.path.join(dirname,filename))}
+    
+def md5_files(dirname):
+    output = {}
 
+    for one_filename in glob.glob(f'{dirname}/*'):
+        try:
+            m = hashlib.md5()
+            m.update(one_filename.encode())
+            output[one_filename] = m.hexdigest()
+        except:
+            pass
+    return output
+        
+def mod_times(dirname):
+    output = {}
 
-print(find_all_longest_words('.'))
+    for one_filename in glob.glob(f'{dirname}/*'):
+        try:
+            mod_time = os.stat(one_filename).ST_MTIME
+            output[one_filename] = (arrow.now() - arrow.get(1503636889)).days
+
+        except:
+            pass
+    return output
+
+def response_counts(filename):
+    output = collection.Counter()
+    for one_line in open(filename):
+        output[one_line.split()[8]] += 1
+    return output  
+
+                    
+print(response_counts("mini_access_log.txt"))
