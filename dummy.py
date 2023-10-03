864f2e535a86fc537156f8aaf3db7b841dfdda4f
hwang <HHwang@qaconsultants.com>
2023-10-03 10:35:41 -0400

    before start ch46

--- dummy.py
+++ dummy.py
@@ -1,61 +1,9 @@
-class Animal():
-    """Base class for animals. Not meant to be instantiated."""
-
-    def __init__(self, color):
-        self.species = self.__class__.__name__
-        self.color = color
-
-    def __repr__(self):
-        return f'{self.color} {self.species}, {self.number_of_legs} legs'
+def foo():
+    yield 1
+    yield 2
+    yield 3
 
 
-class TwoLeggedAnimal(Animal):
-    def __init__(self, color):
-        super().__init__(color)
-        self.number_of_legs = 2
-
-
-class FourLeggedAnimal(Animal):
-    def __init__(self, color):
-        super().__init__(color)
-        self.number_of_legs = 4
-
-
-class ZeroLeggedAnimal(Animal):
-    def __init__(self, color):
-        super().__init__(color)
-        self.number_of_legs = 0
-
-
-class Wolf(FourLeggedAnimal):
-    """Class for creating 4-legged wolves of any color"""
-
-    def __init__(self, color):
-        super().__init__(color)
-
-
-class Sheep(FourLeggedAnimal):
-    """Class for creating 4-legged sheep of any color"""
-
-    def __init__(self, color):
-        super().__init__(color)
-
-
-class Snake(ZeroLeggedAnimal):
-    """Class for creating 0-legged snakes of any color"""
-
-    def __init__(self, color):
-        super().__init__(color)
-
-
-class Parrot(TwoLeggedAnimal):
-    """Class for creating 2-legged parrots of any color"""
-
-    def __init__(self, color):
-        super().__init__(color)
-
-
-print(Wolf("Bbbbbblack"))
-print(Sheep("Whibbbbbte"))
-print(Snake("Rebbbbbd"))
-print(Parrot("Blbbbbbue"))
+g = foo()
+for one_item in g:
+    print(one_item)
