83350c0540f2112866b36af14c00017d695d4728
hwang <HHwang@qaconsultants.com>
2023-10-10 14:58:11 -0400

    after addition

--- 50_example/9_firstlast.py
+++ 50_example/9_firstlast.py
@@ -1,17 +1,18 @@
 def firstlast1(sequence):
     return sequence[:1] + sequence[-1:]
+# print(firstlast1("abcd"))
 
 def odd_even_sum(numbers):
     evens = []
     odds = []
-
     for one_number in numbers:
         if one_number % 2:
             odds.append(one_number)
         else:
             evens.append(one_number)
-
     return [sum(evens), sum(odds)]
+
+# print(odd_even_sum([1,2,3,4,5,6,7,8,9,10]))
 
 """plus_minus([10, 20, 30, 40, 50, 60]), you’ll get back the result of
 10+20-30+40-50+60, or 50."""
@@ -29,6 +30,8 @@ def plus_minus(numbers):
 
 """
 multiple "for comprehensive" can combine 
+myzip([10,20,30], 'abc')
+[(10, 'a'), (20, 'b'), (30, 'c')]
 """
 def myzip(*args):
     print(len(args[0]))
