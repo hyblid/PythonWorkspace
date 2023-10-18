23f529f2be54afc34bb22905566e48ad41b207a4
hwang <HHwang@qaconsultants.com>
2023-10-18 08:22:45 -0400

    before start

--- Udemy_tkinter/gui template.py
+++ Udemy_tkinter/gui template.py
@@ -6,14 +6,14 @@ window = tk.Tk()
 window.title("Demo")
 window.geometry("600x400")
 
-check1 = ttk.Checkbutton(window, text="Check1")
-check2 = ttk.Checkbutton(window, text="Check2")
-radio1 = ttk.Radiobutton(window, text="Radio1")
-radio2 = ttk.Radiobutton(window, text="Radio1")
-check1.pack()
-check2.pack()
-radio1.pack()
-radio2.pack()
+#window minimum
+window.minsize(200, 100)
 
+# screen attributes 
+print(window.winfo_screenwidth())
+print(window.winfo_screenheight())
+
+# security event 
+window.bind('<Escape>', lambda event: window.quit())
 
 window.mainloop()
