3123a59790ee56c68a663d4c4d9f79b89fb171ba
hwang <HHwang@qaconsultants.com>
2023-10-18 20:49:28 -0400

    finished a day

--- Udemy_tkinter/practice.py
+++ Udemy_tkinter/practice.py
@@ -1,47 +1,47 @@
 import tkinter as tk
 from tkinter import ttk
+# import ttkbootstrap as ttk
 
-# window
 window = tk.Tk()
-window.title('More on the window')
-# window.geometry('600x400+100+200')
-window.iconbitmap('python.ico')
+window.title("Demo")
+window.geometry("600x400")
 
-# exercise:
-# start window in the middle of the screen 
-window_width = 1400
-window_height = 600
-display_width = window.winfo_screenwidth()
-display_height = window.winfo_screenheight()
-
-left = int(display_width / 2 - window_width / 2)
-top = int(display_height / 2 - window_height / 2)
-window.geometry(f'{window_width}x{window_height}+{left}+{top}')
-
-# window sizes 
+#window minimum
 window.minsize(200, 100)
-# window.maxsize(800, 700)
-# window.resizable(True,False)
+label1 = ttk.Label(window, text = 'Label 1', background = 'red')
+label2 = ttk.Label(window, text = 'Label 2', background = 'blue')
+"""  side = top or bottom                 |         side= left or right
+   widget canb e as wide as the container |widget canb e as height as the container
+   expand determines height               |expand determines the width 
+"""
+#exapnd : Determines how much space a widget can occupy only in 1 direction!
+#- Width no set by expand if "side" is top or bottom (it already expanded for width)
+#- Height is set by expand but only vertically!
+#fill : Sets how much space the widget will occupy (x,y,both,None)
+#side : determines the direction of the widgets
+label1.pack(side="left", expand= True, fill="y")
+label2.pack(side="left",  expand= True, fill="both")
+
+window.columnconfigure(0, weight =1) # weight is wide
+window.columnconfigure(1, weight =1)
+window.columnconfigure(2, weight =2)
+window.rowconfigure(0, weight=1)
+window.rowconfigure(1, weight=1)
+
+label1.grid(row=0, column=1, sticky="nsew") #stick with 4 directions border
+label2.grid(row=1, column=1, columnspan =2, sticky="nsew")
+ 
+
+
 
 # screen attributes 
 print(window.winfo_screenwidth())
 print(window.winfo_screenheight())
 
-# window attributes
-window.attributes('-alpha', 1)
-# window.attributes('-topmost', True)
-
 # security event 
 window.bind('<Escape>', lambda event: window.quit())
 
-# window.attributes('-disable', True)
-# window.attributes('-fullscreen', True)
 
 
-# title bar 
-window.overrideredirect(True)
-grip = ttk.Sizegrip(window)
-grip.place(relx = 1.0, rely = 1.0, anchor = 'se')
 
-# run
 window.mainloop()
