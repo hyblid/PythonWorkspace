f99f3de1d3b8d09490bd3550cc3f08068ac8de84
Howard Hwang <hhwang@qac.com>
2023-11-16 22:44:43 -0500

    finish a day

---
+++ Udemy_tkinter/menu.py
@@ -0,0 +1,31 @@
+import customtkinter as ctk
+from resources.image_editor_setting import *
+from panels import *
+
+class Menu(ctk.CTkTabview):
+    def __init__(self, parent):
+        super().__init__(master=parent)
+        self.grid(row=0, column =0, sticky="news", pady = 10, padx=10)
+        
+        #tabs
+        self.add("Position")
+        self.add("Color")
+        self.add("Effects")
+        self.add("Export")
+        #widgets
+        PositionFrame(self.tab("Position"))
+        ColorFrame(self.tab("Color"))
+        
+
+class PositionFrame(ctk.CTkFrame):
+    def __init__(self, parent):
+        super().__init__(master=parent, fg_color="transparent")
+        self.pack(expand=True, fill="both")
+        SliderPanel(self, "Rotation")
+        SliderPanel(self, "Zoom")
+        
+
+class ColorFrame(ctk.CTkFrame):
+    def __init__(self, parent):
+        super().__init__(master=parent, fg_color="transparent")
+        self.pack(expand=True, fill="both")
