59bef1b4f3a403256c28241e70230d6c93c51a78
Howard Hwang <hhwang@qac.com>
2023-11-17 15:52:28 -0500

    finish a day

--- Udemy_tkinter/main.py
+++ Udemy_tkinter/main.py
@@ -1,7 +1,7 @@
 import customtkinter as ctk
 from resources.image_editor_setting import *
 from image_widgets import*
-from PIL import Image, ImageTk
+from PIL import Image, ImageTk, ImageOps, ImageEnhance, ImageFilter
 from menu import Menu
 
 class App(ctk.CTk):
@@ -11,19 +11,89 @@ class App(ctk.CTk):
         self.geometry("1000x600")
         self.title("Photo Editor")
         self.minsize(800,500)
+        self.init_parameters()
 
         #layout
         self.rowconfigure(0, weight=1)
         self.columnconfigure(0, weight=2, uniform = "a")
         self.columnconfigure(1, weight=6, uniform= "a")
         
-        
+        #canvas data
+        self.image_width=0
+        self.image_height=0
+        self.canvas_width=0
+        self.canvas_width=0
         #widgets
         self.image_import = ImageImport(self, self.import_image)
         #importButton(Frame with a button)
         self.mainloop()
     
+    def init_parameters(self):
+        self.pos_vars = {
+            "rotate": ctk.DoubleVar(value=ROTATE_DEFAULT),
+            "zoom" : ctk.DoubleVar(value=ZOOM_DEFAULT),
+            "flip" : ctk.StringVar(value = FLIP_OPTIONS[0])}
+        self.color_vars = {
+            "brightness": ctk.DoubleVar(value=BRIGHTNESS_DEFAULT),
+            "grayscale" : ctk.BooleanVar(value=GRAYSCALE_DEFAULT),
+            "invert" : ctk.BooleanVar(value = INVERT_DEFAULT),
+            "vibrance" : ctk.DoubleVar(value = VIBRANCE_DEFAULT)
+        }
+        self.effect_vars = {
+            "blur" : ctk.DoubleVar(value=BLUR_DEFAULT),
+            "contrast" : ctk.IntVar(value=CONTRAST_DEFAULT),
+            "effect" : ctk.StringVar(value=EFFECT_OPTIONS[0]),
+        }
+        #tracinng
+        combined_vars = list(self.pos_vars.values()) + list(self.color_vars.values()) + list(self.effect_vars.values())
+        for var in combined_vars:
+            var.trace("w", self.manuplate_image)
+
+    def manuplate_image(self, *args):
+        self.image = self.original
+        
+        #rotate
+        if self.pos_vars["rotate"].get() != ROTATE_DEFAULT:
+            self.image = self.image.rotate(self.pos_vars['rotate'].get())
+        #zoom
+        if self.pos_vars["zoom"].get() != ZOOM_DEFAULT:
+            self.image = ImageOps.crop(image = self.image, border = self.pos_vars['zoom'].get())
+        #flip
+        if self.pos_vars["flip"].get() != FLIP_OPTIONS[0]:
+            if self.pos_vars["flip"].get() == "X":
+                self.image = ImageOps.mirror(self.image)
+            if self.pos_vars["flip"].get() == "Y":
+                self.image = ImageOps.flip(self.image)
+            if self.pos_vars["flip"].get() == "Both":
+                self.image = ImageOps.mirror(self.image)
+                self.image = ImageOps.flip(self.image)
+        #brightness & varance
+        if self.color_vars["brightness"].get() != BRIGHTNESS_DEFAULT:
+            brightness_enhancer = ImageEnhance.Brightness(self.image)
+            self.image =brightness_enhancer.enhance(self.color_vars["brightness"].get())
+        if self.color_vars["vibrance"].get() != VIBRANCE_DEFAULT:
+            vibrance_enhancer = ImageEnhance.Color(self.image)
+            self.image =vibrance_enhancer.enhance(self.color_vars["vibrance"].get())
+        #color
+        if self.color_vars["grayscale"].get():
+            self.image = ImageOps.grayscale(self.image)
+        if self.color_vars["invert"].get():
+            self.image = ImageOps.invert(self.image)
+        #blur & contrast
+        if self.effect_vars["blur"].get() != BLUR_DEFAULT:      
+            self.image = self.image.filter(ImageFilter.GaussianBlur(self.effect_vars["blur"].get()))
+        if self.effect_vars["contrast"].get() != CONTRAST_DEFAULT:      
+            self.image = self.image.filter(ImageFilter.UnsharpMask(self.effect_vars["contrast"].get()))
+        match self.effect_vars["effect"].get():
+            case "Emboss": self.image= self.image.filter(ImageFilter.EMBOSS)
+            case "Find edges": self.image= self.image.filter(ImageFilter.FIND_EDGES)
+            case "Contour": self.image= self.image.filter(ImageFilter.CONTOUR)
+            case "Edge enhance": self.image= self.image.filter(ImageFilter.EDGE_ENHANCE)
+        
+        self.place_image()
+        
     def import_image(self, path):
+        self.original = Image.open(path)
         self.image = Image.open(path)
         self.image_ratio =self.image.size[0]/ self.image.size[1]
         self.image_tk = ImageTk.PhotoImage(self.image)
@@ -31,7 +101,7 @@ class App(ctk.CTk):
         self.image_import.grid_forget()
         self.image_output = ImageOutput(self, self.resize_image)
         self.close_button = CloseOutput(self, self.close_edit)
-        self.menu = Menu(self)
+        self.menu = Menu(self, self.pos_vars, self.color_vars, self.effect_vars)
         
     def close_edit(self):
         #hide the image and the colose button
@@ -40,25 +110,28 @@ class App(ctk.CTk):
         self.menu.grid_forget()
         #recreate the import button
         self.image_import = ImageImport(self, self.import_image)
-    
         
     def resize_image(self,event):
         #current canvas raio
         canvas_ratio = event.width / event.height
+        #update canvas attivutes
+        self.canvas_width = event.width
+        self.canvas_height = event.height
         
         #resize
         if canvas_ratio > self.image_ratio: #canvas is wider than the image
-            image_height = int(event.height)
-            image_width = int(image_height * self.image_ratio)
+            self.image_height = int(event.height)
+            self.image_width = int(self.image_height * self.image_ratio)
         else: #canvas is taller than the image
-            image_width = int(event.width)
-            image_height = int(image_width / self.image_ratio)
+            self.image_width = int(event.width)
+            self.image_height = int(self.image_width / self.image_ratio)
+        
+        self.place_image()
             
-            
-        #place image
+    def place_image(self): 
         self.image_output.delete("all")
-        resized_image = self.image.resize((image_width, image_height))
+        resized_image = self.image.resize((self.image_width, self.image_height))
         self.image_tk =  ImageTk.PhotoImage(resized_image)       
-        self.image_output.create_image(event.width/2,event.height/2, image = self.image_tk)    
+        self.image_output.create_image(self.canvas_width/2, self.canvas_height/2, image = self.image_tk)    
 
 App()    
